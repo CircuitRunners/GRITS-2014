67e28c82dab481c0c2ee10c977727fe8fb6824fd
Akilan Narayanaswamy <akilan98@gmail.com>
2014-09-11 18:23:49 -0400

    Progress 9/11/14.
    Variables and objects - RobotDrive, Talon, Joystick.
    operatorControl() - while loop

--- src/org/github/CircuitRunners/GRITS2014.java
+++ src/org/github/CircuitRunners/GRITS2014.java
@@ -8,7 +8,10 @@
 package org.github.CircuitRunners;
 
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Talon;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,6 +21,29 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class GRITS2014 extends SimpleRobot {
+    
+    //Robot Drive
+    RobotDrive drive;
+    
+    // Talons
+    public Talon leftFront;
+    public Talon leftBack;
+    public Talon rightFront;
+    public Talon rightBack;
+    
+    // Joystick
+    public Joystick xbone;
+
+    public GRITS2014() {
+        leftFront = new Talon(1);
+        leftBack = new Talon(2);
+        rightFront = new Talon(3);
+        rightBack = new Talon(4);
+        
+        drive = new RobotDrive(leftFront, leftBack, rightFront, rightBack);
+        
+        xbone = new Joystick(1);
+    }
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
@@ -29,13 +55,8 @@ public class GRITS2014 extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
-    }
-    
-    /**
-     * This function is called once each time the robot enters test mode.
-     */
-    public void test() {
-    
+        while(isOperatorControl()){
+            
+        }
     }
 }
